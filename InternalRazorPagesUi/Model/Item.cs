ce522d693282d3236f35a8223abc9e45926bb3ef
HenrikDK <commander.john.crichton@gmail.com>
2021-12-18 00:56:22 +0100

    add model object for table content

---
+++ InternalRazorPagesUi/Model/Item.cs
@@ -0,0 +1,14 @@
+namespace InternalRazorPagesUi.Model;
+
+public class Item
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string Gender { get; set; }
+    public int Rating { get; set; }
+    public string Color { get; set; }
+    public int Progress { get; set; }
+    public string DateOfBirth { get; set; }
+    public bool Driver { get; set; }
+}
