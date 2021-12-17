9cd4ed6079143ee6e66b4cea30c99944c4b6bdf7
HenrikDK <commander.john.crichton@gmail.com>
2021-12-18 00:55:00 +0100

    javascript for side bar persistence

--- InternalRazorPagesUi/wwwroot/js/site.js
+++ InternalRazorPagesUi/wwwroot/js/site.js
@@ -1,4 +1,22 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$( document ).ready(function() {
+    restoreSidebarMenu();
+});
 
-// Write your JavaScript code.
+function persistSideBarState() {
+    var menuItems = $('.menu-toggle').map(function () {
+        return { 'target': $(this).data('bs-target'), 'aria': this.ariaExpanded}
+    }).get();
+    
+    window.localStorage.setItem('side-bar-expand-state', JSON.stringify(menuItems));
+}
+
+function restoreSidebarMenu() {
+    var menuItems = JSON.parse(window.localStorage.getItem('side-bar-expand-state'))
+
+    $('.menu-toggle').each(function (index) {
+        if ($(this).data('bs-target') === menuItems[index]['target'] && this.ariaExpanded !== menuItems[index]['aria'])
+        {
+            this.click()
+        }
+    });
+}
