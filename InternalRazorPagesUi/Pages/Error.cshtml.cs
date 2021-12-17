5c7d6887f995bb4f2b1366fdf77051c055c0f9f8
HenrikDK <commander.john.crichton@gmail.com>
2021-12-18 00:57:42 +0100

    fix namespace in pages

--- InternalRazorPagesUi/Pages/Error.cshtml.cs
+++ InternalRazorPagesUi/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace internal_razor_pages_ui.Pages;
+namespace InternalRazorPagesUi.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
