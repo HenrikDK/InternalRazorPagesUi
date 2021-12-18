390aa434fd40cae1183dc9039018574ac7819bbd
HenrikDK <commander.john.crichton@gmail.com>
2021-12-18 01:03:36 +0100

    silence compiler warnings

--- InternalRazorPagesUi/Model/Item.cs
+++ InternalRazorPagesUi/Model/Item.cs
@@ -3,12 +3,12 @@ namespace InternalRazorPagesUi.Model;
 public class Item
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Location { get; set; }
-    public string Gender { get; set; }
+    public string Name { get; set; } = "";
+    public string Location { get; set; } = "";
+    public string Gender { get; set; } = "";
     public int Rating { get; set; }
-    public string Color { get; set; }
+    public string Color { get; set; } = "";
     public int Progress { get; set; }
-    public string DateOfBirth { get; set; }
+    public string DateOfBirth { get; set; } = "";
     public bool Driver { get; set; }
 }
