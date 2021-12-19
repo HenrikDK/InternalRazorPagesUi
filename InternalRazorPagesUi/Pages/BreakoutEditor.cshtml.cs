7fbed6baff49b12ca7c864b40efc3fb682db58a5
HenrikDK <commander.john.crichton@gmail.com>
2021-12-19 22:26:18 +0100

    initial breakout editor

---
+++ InternalRazorPagesUi/Pages/BreakoutEditor.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using InternalRazorPagesUi.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InternalRazorPagesUi.Pages;
+
+public class BreakoutEditorModel : PageModel
+{
+    private readonly ILogger<BreakoutEditorModel> _logger;
+
+    public BreakoutEditorModel(ILogger<BreakoutEditorModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public EditorMode EditMode { get; set; }
+
+    public IActionResult OnGet(string url)
+    {
+        EditMode = EditorMode.Edit;
+        
+        return Page();
+    }
+}
