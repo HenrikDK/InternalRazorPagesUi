b4dc382f8c513534ecc907e41ca02a5d1bd94433
HenrikDK <commander.john.crichton@gmail.com>
2021-12-19 22:25:42 +0100

    add validation for breakout editor

--- InternalRazorPagesUi/Model/Item.cs
+++ InternalRazorPagesUi/Model/Item.cs
@@ -1,15 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace InternalRazorPagesUi.Model;
 
 public class Item
 {
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; } = "";
+    [Required]
     public string Location { get; set; } = "";
+    [Required]
     public string Gender { get; set; } = "";
+    [Required]
     public int Rating { get; set; }
+    [Required]
     public string Color { get; set; } = "";
+    
     public int Progress { get; set; }
+    [Required]
     public string DateOfBirth { get; set; } = "";
+    
     public bool Driver { get; set; }
+    
     public bool isDelete { get; set; } = false;
 }
