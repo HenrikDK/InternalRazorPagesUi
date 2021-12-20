7bf354356de246daf1e772ceaa71800456800a70
HenrikDK <commander.john.crichton@gmail.com>
2021-12-20 02:09:06 +0100

    revert to old structure since new webapp builder doesnt play nicely with lamar

--- InternalRazorPagesUi/Program.cs
+++ InternalRazorPagesUi/Program.cs
@@ -1,34 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
+using InternalRazorPagesUi;
+using Lamar.Microsoft.DependencyInjection;
+using Microsoft.AspNetCore;
 
-// Add services to the container.
-builder.Services
-    .AddRazorPages()
-    .AddRazorPagesOptions(options =>
-    {
-        options.Conventions.AddPageRoute("/ReverseProxy", "sp/{*url}");
-        options.Conventions.AddPageRoute("/ReverseProxy", "back-office/sp/{*url}");
-    });
+var builder = WebHost.CreateDefaultBuilder(args);
 
-builder.WebHost.UseUrls("http://*:80/");
+var host = builder
+    .UseKestrel()
+    .UseLamar()
+    .UseUrls("http://*:80/")
+    .UseStartup<Startup>()
+    .Build();
 
-var app = builder.Build();
-
-app.UseStatusCodePages();
-app.UseStatusCodePagesWithReExecute("/Status{0}");
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseStaticFiles();
-
-app.UseRouting();
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
+host.Run();
