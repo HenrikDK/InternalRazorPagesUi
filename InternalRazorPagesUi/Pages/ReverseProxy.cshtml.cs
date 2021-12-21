97555b460e7c60fad1ebe46cd3a8aaae3ca218cb
HenrikDK <commander.john.crichton@gmail.com>
2021-12-21 02:22:52 +0100

    rework code behind to handle the return of files better

--- InternalRazorPagesUi/Pages/ReverseProxy.cshtml.cs
+++ InternalRazorPagesUi/Pages/ReverseProxy.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using InternalRazorPagesUi.ReverseProxy;
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,49 +8,67 @@ namespace InternalRazorPagesUi.Pages;
 
 public class ReverseProxyModel : PageModel
 {
-    public string ResponseBody { get; set; } = "";
-    private readonly ILogger<ReverseProxyModel> _logger;
-    private readonly IReverseProxy _reverseProxy;
+    public string Title { get; set; }
+    public HtmlString ResponseBody { get; set; }
     
-    public ReverseProxyModel(ILogger<ReverseProxyModel> logger, IReverseProxy reverseProxy)
+    private readonly ILogger<ReverseProxyModel> _logger;
+    private readonly IProcessResponse _processResponse;
+    private readonly IProxyRequest _proxyRequest;
+    
+    public ReverseProxyModel(ILogger<ReverseProxyModel> logger,
+        IProcessResponse processResponse,
+        IProxyRequest proxyRequest)
     {
         _logger = logger;
-        _reverseProxy = reverseProxy;
+        _processResponse = processResponse;
+        _proxyRequest = proxyRequest;
     }
     
     public async Task<ActionResult> OnGet()
     {
-        var response = await _reverseProxy.ProxyRequestToService(HttpContext);
+        var (proxyRequest, response) = await _proxyRequest.RequestFromService(HttpContext);
         if (response.StatusCode == HttpStatusCode.NotFound)
         {
             return new StatusCodeResult(404);
         }
 
-        ResponseBody = response.Content.ToString() ?? "";
-        ResponseBody = ExtractBody(ResponseBody);
-        
-        return Page();
+        var result = await _processResponse.ProcessAndExtract(HttpContext, response, proxyRequest);
+        if (result.ContentType == ContentType.TextHtml)
+        {
+            Title = result.Title;
+            ResponseBody = result.Body;
+            return Page();
+        }
+
+        if (result.ContentType == ContentType.TextJs)
+        {
+            return new FileContentResult(result.Bytes, "text/javascript");    
+        }
+
+        return new FileContentResult(result.Bytes, response.Content.Headers.ContentType?.MediaType ?? "text/json");
     }
 
     public async Task<ActionResult> OnPost()
     {
-        var response = await _reverseProxy.ProxyRequestToService(HttpContext);
+        var (proxyRequest, response) = await _proxyRequest.RequestFromService(HttpContext);
         if (response.StatusCode == HttpStatusCode.NotFound)
         {
             return new StatusCodeResult(404);
         }
 
-        ResponseBody = response.Content.ToString() ?? "";
-        ResponseBody = ExtractBody(ResponseBody);
+        var result = await _processResponse.ProcessAndExtract(HttpContext, response, proxyRequest);
+        if (result.ContentType == ContentType.TextHtml)
+        {
+            Title = result.Title;
+            ResponseBody = result.Body;
+            return Page();
+        }
 
-        return Page();
-    }
+        if (result.ContentType == ContentType.TextJs)
+        {
+            return new FileContentResult(result.Bytes, "text/javascript");    
+        }
 
-    private string ExtractBody(string response)
-    {
-        if (string.IsNullOrEmpty(response)) return response;
-        
-        var body = response[(response.IndexOf("<body") + 6)..(response.IndexOf("</body") - 1)];
-        return body;
+        return new FileContentResult(result.Bytes, response.Content.Headers.ContentType?.MediaType ?? "text/json");
     }
 }
