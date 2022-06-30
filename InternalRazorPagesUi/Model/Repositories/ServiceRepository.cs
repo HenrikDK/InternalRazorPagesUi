f44f8ba7efd10b19a16ddc31f0c542e59385acb5
HenrikDK <commander.john.crichton@gmail.com>
2022-06-30 19:01:13 +0200

    simplify caching theres no need for a singleton memory cache is a service

--- InternalRazorPagesUi/Model/Repositories/ServiceRepository.cs
+++ InternalRazorPagesUi/Model/Repositories/ServiceRepository.cs
@@ -2,18 +2,34 @@ namespace InternalRazorPagesUi.Model.Repositories;
 
 public interface IServiceRepository
 {
-    IDictionary<string, string> Execute();
+    IDictionary<string, string> GetAll();
 }
     
 public class ServiceRepository : IServiceRepository
-{ 
+{
+    private readonly IMemoryCache _cache;
+
+    public ServiceRepository(IMemoryCache cache)
+    {
+        _cache = cache;
+    }
+    
     List<(string service, string url)> _services = new()
     {
         ("Orders", "http://localhost:9080"),
         ("Inventory", "http://localhost:9080")
     };
     
-    public IDictionary<string, string> Execute()
+    public IDictionary<string, string> GetAll()
+    {
+        return _cache.GetOrCreate("services", x =>
+        {
+            x.AbsoluteExpiration = DateTime.Now.AddMinutes(15);
+            return GetAllFromService();
+        });
+    }
+    
+    private IDictionary<string, string> GetAllFromService()
     {
         return _services.ToDictionary(x => x.service, x => x.url);
     }
